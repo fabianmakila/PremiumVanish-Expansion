7cac1f2eb321a094acfd004e41913f1f2e322ec2
Fabian Mäkilä <contact@fabianadrian.fi>
2025-05-21 11:02:59 +0300

    finish translations

--- velocity/src/main/java/io/github/miniplaceholders/expansion/premiumvanish/velocity/VelocityPlugin.java
+++ velocity/src/main/java/io/github/miniplaceholders/expansion/premiumvanish/velocity/VelocityPlugin.java
@@ -3,6 +3,8 @@ package io.github.miniplaceholders.expansion.premiumvanish.velocity;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.proxy.ProxyReloadEvent;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import io.github.miniplaceholders.api.Expansion;
@@ -10,21 +12,26 @@ import io.github.miniplaceholders.expansion.premiumvanish.velocity.locale.Transl
 import io.github.miniplaceholders.expansion.premiumvanish.velocity.placeholder.*;
 import org.slf4j.Logger;
 
+import java.nio.file.Path;
+
 public final class VelocityPlugin {
 	private final Logger logger;
 	private final ProxyServer server;
+	private final TranslationManager translationManager;
 
 	@Inject
-	public VelocityPlugin(final ProxyServer server, final Logger logger) {
+	public VelocityPlugin(final ProxyServer server, final @DataDirectory Path dataDirectory, final Logger logger) {
 		this.server = server;
 		this.logger = logger;
+
+		this.translationManager = new TranslationManager(dataDirectory, logger);
 	}
 
 	@Subscribe
 	public void onProxyInitialize(final ProxyInitializeEvent event) {
 		this.logger.info("Starting PremiumVanish Expansion for Velocity");
 
-		new TranslationManager(this.logger);
+		this.translationManager.load();
 
 		Expansion.builder("premiumvanish")
 				.filter(Player.class)
@@ -37,5 +44,10 @@ public final class VelocityPlugin {
 				.audiencePlaceholder("seelevel", new UseLevelPlaceholder())
 				.build()
 				.register();
+	}
+
+	@Subscribe
+	public void onProxyReload(final ProxyReloadEvent event) {
+		this.translationManager.load();
 	}
 }
--- velocity/src/main/java/io/github/miniplaceholders/expansion/premiumvanish/velocity/locale/TranslationManager.java
+++ velocity/src/main/java/io/github/miniplaceholders/expansion/premiumvanish/velocity/locale/TranslationManager.java
@@ -3,32 +3,107 @@ package io.github.miniplaceholders.expansion.premiumvanish.velocity.locale;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.minimessage.translation.MiniMessageTranslationStore;
 import net.kyori.adventure.translation.GlobalTranslator;
+import net.kyori.adventure.translation.Translator;
 import org.slf4j.Logger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Locale;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
+import java.util.StringJoiner;
 
 public final class TranslationManager {
 	public static final Locale DEFAULT_LOCALE = Locale.ENGLISH;
 	private final Logger logger;
-	private final MiniMessageTranslationStore store;
+	private final Path localeDirectoryPath;
+	private MiniMessageTranslationStore store;
 
-	public TranslationManager(Logger logger) {
+	public TranslationManager(Path path, Logger logger) {
+		this.localeDirectoryPath = path.resolve("locale");
 		this.logger = logger;
+	}
+
+	public void load() {
+		if (this.store != null) {
+			GlobalTranslator.translator().removeSource(this.store);
+		}
 
 		this.store = MiniMessageTranslationStore.create(Key.key("miniplaceholders", "premiumvanish"));
 		this.store.defaultLocale(DEFAULT_LOCALE);
 
-		loadFromResourceBundle();
+		createLocaleDirectory();
+		copyDefaultBundleToLocaleDirectory();
+		loadFromLocaleDirectory();
+
 		GlobalTranslator.translator().addSource(this.store);
 	}
 
-	private void loadFromResourceBundle() {
+	private void createLocaleDirectory() {
 		try {
-			ResourceBundle bundle = ResourceBundle.getBundle("messages", DEFAULT_LOCALE);
-			this.store.registerAll(DEFAULT_LOCALE, bundle, false);
-		} catch (IllegalArgumentException e) {
-			this.logger.warn("Error loading default locale file", e);
+			Files.createDirectory(this.localeDirectoryPath);
+		} catch (IOException e) {
+			this.logger.error("Couldn't create locale directory", e);
 		}
+	}
+
+	private void copyDefaultBundleToLocaleDirectory() {
+		Path defaultBundlePath = this.localeDirectoryPath.resolve("messages_en.properties");
+		if (Files.exists(defaultBundlePath)) {
+			return;
+		}
+
+		try {
+			Files.copy(this.getClass().getClassLoader().getResourceAsStream("messages_en.properties"), defaultBundlePath);
+		} catch (IOException e) {
+			this.logger.error("Could not write default bundle", e);
+		}
+	}
+
+	private void loadFromLocaleDirectory() {
+		StringJoiner loadedLocaleNamesJoiner = new StringJoiner(", ");
+
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(this.localeDirectoryPath, "*.properties")) {
+			for (Path localeFilePath : stream) {
+				Path fileNamePath = localeFilePath.getFileName();
+				if (!fileNamePath.startsWith("messages_")) {
+					this.logger.warn("Couldn't load {}. Locale files must follow specified naming convention", fileNamePath);
+					continue;
+				}
+
+				Locale locale = parseLocaleFromFileName(localeFilePath);
+				loadedLocaleNamesJoiner.add(locale.getLanguage());
+
+				ResourceBundle bundle;
+				try (BufferedReader reader = Files.newBufferedReader(localeFilePath, StandardCharsets.UTF_8)) {
+					bundle = new PropertyResourceBundle(reader);
+				}
+
+				this.store.registerAll(locale, bundle, false);
+			}
+
+			if (loadedLocaleNamesJoiner.length() != 0) {
+				this.logger.info("Loaded locales: {}", loadedLocaleNamesJoiner);
+			}
+		} catch (IOException e) {
+			this.logger.warn("Couldn't read the locale directory", e);
+		}
+	}
+
+	private Locale parseLocaleFromFileName(Path localeFilePath) {
+		String fileName = localeFilePath.getFileName().toString();
+		String localeString = fileName.substring(
+				"messages_".length(),
+				fileName.length() - ".properties".length()
+		);
+		Locale locale = Translator.parseLocale(localeString);
+		if (locale == null) {
+			throw new IllegalStateException("Couldn't parse locale for file name: " + fileName);
+		}
+		return locale;
 	}
 }
